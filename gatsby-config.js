8106172ec54dfb5505f60e1f2f1c536784a896c6
Martin Makarský <martinm@kentico.com>
2019-11-21 13:36:25 +0100

    Update gatsby-config.js
    Set preview API key.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,12 @@ module.exports = {
       resolve: `@kentico/gatsby-source-kontent`,
       options: {
         deliveryClientConfig: {
-          projectId: `479c94ca-8760-00f5-6b07-15d19b791204`,
+          projectId: '479c94ca-8760-00f5-6b07-15d19b791204',
+          globalQueryConfig: {
+            usePreviewMode: true,
+          },
+          previewApiKey: 'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAianRpIjogIjAxNjAzMTFkOWU1ZTQ2MzI4MGEyMzU3ZWNlOWJjNjBmIiwNCiAgImlhdCI6ICIxNTc0MjUxMDY1IiwNCiAgImV4cCI6ICIxOTE5ODUxMDY1IiwNCiAgInByb2plY3RfaWQiOiAiNDc5Yzk0Y2E4NzYwMDBmNTZiMDcxNWQxOWI3OTEyMDQiLA0KICAidmVyIjogIjEuMC4wIiwNCiAgImF1ZCI6ICJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSINCn0.zaBcER3ZD8n83lLNXdz2b-vrFrtgn81oJqoy9-D-QJE',
+          typeResolvers: []
         },
         languageCodenames: [
           `default`,
